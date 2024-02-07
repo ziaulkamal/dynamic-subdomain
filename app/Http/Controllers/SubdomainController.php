90e1d02b6ee43dfb555a2b9092b627b5240a5849
Ziaul Kamal <codepad@usa.com>
2024-02-07 13:21:44 +0700

    ok

--- app/Http/Controllers/SubdomainController.php
+++ app/Http/Controllers/SubdomainController.php
@@ -123,7 +123,7 @@ class SubdomainController extends Controller
             $text = $responseData['candidates'][0]['content']['parts'][0]['text'];
 
             // Kirim hasil sebagai variabel ke blade show
-            return view('show', compact('subdomain', 'text', 'query'));
+            return redirect()->route('subdomain.show', ['subdomain' => $subdomain])->with(compact('text', 'query'));
         }
 
         // Tutup koneksi cURL
