c2a6bb37226dec72a7db8f2b8f1c79cfaa590f9d
Ziaul Kamal <codepad@usa.com>
2024-02-07 13:49:59 +0700

    ok

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Show Subdomain</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 <body>
     <h1>Informasi Subdomain:</h1>
@@ -15,15 +16,10 @@
     @endif
 
     <!-- Tambahkan hasil query di sini -->
-    @if (isset($text))
+       <div id="resultContainer">
         <h2>Hasil Query:</h2>
-        <pre id="resultText">{{ $text }}</pre>
-    @elseif (isset($subdomain))
-        <h2>Hasil Query:</h2>
-        <p>File JSON tidak ditemukan untuk subdomain {{ $subdomain }}.</p>
-    @else
-        <p>Terjadi kesalahan dalam mendapatkan hasil dari API.</p>
-    @endif
+        <pre id="resultText"></pre>
+    </div>
 
     <form action="{{ route('process.query.post') }}" method="POST">
         @csrf
@@ -33,49 +29,39 @@
     </form>
 
     <script>
+        var fullUrl = window.location.href;
 
+        // Hapus protokol dari URL
+        var urlWithoutProtocol = fullUrl.replace(/(^\w+:|^)\/\//, '');
 
-        // Fungsi untuk mengirim permintaan AJAX saat halaman dimuat
-        window.addEventListener('DOMContentLoaded', function() {
-            submitForm();
+        // Ambil protokol dari URL
+        var protocol = window.location.protocol + '//';
+
+        // Pisahkan URL berdasarkan titik (.)
+        var urlParts = urlWithoutProtocol.split('.');
+
+        // Ambil subdomain yang pertama (indeks 0)
+        var subdomain = urlParts[0];
+        var rootDomain = urlParts.length > 2 ? urlParts[1] + '.' + urlParts[2] : urlParts[0];
+        // Persiapkan path untuk file JSON
+        // Ambil protokol dan path untuk file JSON
+        var protocol = window.location.protocol;
+        var jsonFilePath = protocol + '//' + rootDomain + '/responses/' + subdomain + '.json';
+
+        // Lakukan permintaan AJAX
+        $.ajax({
+            url: jsonFilePath,
+            type: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                // Tampilkan hasil di elemen dengan id 'resultText'
+                $('#resultText').text(response.candidates[0].content.parts[0].text);
+            },
+            error: function() {
+                // Tangani kesalahan jika permintaan gagal
+                $('#resultText').text('Terjadi kesalahan dalam mendapatkan hasil dari API.');
+            }
         });
-
-        function submitForm() {
-        // Ambil URL lengkap dari browser
-
-            var fullUrl = window.location.href;
-
-            // Hapus protokol dari URL
-            var urlWithoutProtocol = fullUrl.replace(/(^\w+:|^)\/\//, '');
-
-            // Ambil protokol dari URL
-            var protocol = window.location.protocol + '//';
-
-            // Pisahkan URL berdasarkan titik (.)
-            var urlParts = urlWithoutProtocol.split('.');
-
-            // Ambil subdomain yang pertama (indeks 0)
-            var subdomain = urlParts[0];
-            var rootDomain = urlParts.length > 2 ? urlParts[1] + '.' + urlParts[2] : urlParts[0];
-            // Persiapkan path untuk file JSON
-            var jsonFilePath = protocol +rootDomain + 'responses/' + subdomain + '.json';
-
-            // Menggunakan AJAX untuk memuat file JSON
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        // File JSON berhasil dimuat, tampilkan hasil pada elemen dengan id 'resultText'
-                        document.getElementById('resultText').innerText = JSON.parse(xhr.responseText).candidates[0].content.parts[0].text;
-                    } else {
-                        // File JSON tidak ditemukan, tampilkan pesan
-                        document.getElementById('resultText').innerText = 'File JSON tidak ditemukan untuk subdomain ' + subdomain + '.';
-                    }
-                }
-            };
-            xhr.open('GET', jsonFilePath, true);
-            xhr.send();
-        }
     </script>
 </body>
 </html>
