a155e1ca68b07ec9a63399ffe15e465d5d0dff49
Ziaul Kamal <codepad@usa.com>
2024-02-07 13:39:36 +0700

    ok

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -17,7 +17,10 @@
     <!-- Tambahkan hasil query di sini -->
     @if (isset($text))
         <h2>Hasil Query:</h2>
-        <pre>{{ $text }}</pre>
+        <pre id="resultText">{{ $text }}</pre>
+    @elseif (isset($subdomain))
+        <h2>Hasil Query:</h2>
+        <p>File JSON tidak ditemukan untuk subdomain {{ $subdomain }}.</p>
     @else
         <p>Terjadi kesalahan dalam mendapatkan hasil dari API.</p>
     @endif
@@ -28,5 +31,46 @@
         <input type="text" id="query" name="query" placeholder="Contoh: subdomain1" required>
         <button type="submit">Submit</button>
     </form>
+
+    <script>
+
+
+        // Fungsi untuk mengirim permintaan AJAX saat halaman dimuat
+        window.addEventListener('DOMContentLoaded', function() {
+            submitForm();
+        });
+
+        function submitForm() {
+        // Ambil URL lengkap dari browser
+            var fullUrl = window.location.href;
+
+            // Hapus protokol dari URL
+            var urlWithoutProtocol = fullUrl.replace(/(^\w+:|^)\/\//, '');
+
+            // Pisahkan URL berdasarkan titik (.)
+            var urlParts = urlWithoutProtocol.split('.');
+
+            // Ambil subdomain yang pertama (indeks 0)
+            var subdomain = urlParts[0];
+            // Persiapkan path untuk file JSON
+            var jsonFilePath = '/responses/' + subdomain + '.json';
+
+            // Menggunakan AJAX untuk memuat file JSON
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        // File JSON berhasil dimuat, tampilkan hasil pada elemen dengan id 'resultText'
+                        document.getElementById('resultText').innerText = JSON.parse(xhr.responseText).candidates[0].content.parts[0].text;
+                    } else {
+                        // File JSON tidak ditemukan, tampilkan pesan
+                        document.getElementById('resultText').innerText = 'File JSON tidak ditemukan untuk subdomain ' + subdomain + '.';
+                    }
+                }
+            };
+            xhr.open('GET', jsonFilePath, true);
+            xhr.send();
+        }
+    </script>
 </body>
 </html>
