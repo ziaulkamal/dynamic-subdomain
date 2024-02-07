2055369582e05c012daddc5bc3cc4d55d85b647a
Ziaul Kamal <codepad@usa.com>
2024-02-07 11:26:10 +0700

    ok

--- app/Http/Controllers/SubdomainController.php
+++ app/Http/Controllers/SubdomainController.php
@@ -6,11 +6,17 @@ use Illuminate\Http\Request;
 
 class SubdomainController extends Controller
 {
-    public function index(Request $request)
+    public function showSubdomain(Request $request, $subdomain = null)
     {
-        $subdomain = $request->subdomain;
-        // Logika untuk subdomain di sini
-        return view('subdomain.show', compact('subdomain'));
+        // Jika ada subdomain, tampilkan view subdomain
+        if ($subdomain) {
+            return view('show', compact('subdomain'));
+        }
+
+        // Jika tidak ada subdomain, tampilkan posisi sekarang
+        $currentSubdomain = $request->subdomain;
+        return view('show', compact('currentSubdomain'));
     }
+
 
 }
