7b5e1c7a3c20692f79fec33b3598d6d77e44b8a5
Ziaul Kamal <codepad@usa.com>
2024-02-07 11:32:25 +0700

    ok

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -16,15 +16,12 @@
         <p>URL Sekarang: {{ url('/') }}</p>
     @endif
 
-    {{-- @if (isset($subdomain))
-        <p><a href="{{ route('root') }}">Kembali ke Root</a></p>
-    @endif
 
     <form action="{{ route('process.query.post') }}" method="POST">
         @csrf
         <label for="query">Masukkan Query:</label>
         <input type="text" id="query" name="query" placeholder="Contoh: subdomain1" required>
         <button type="submit">Submit</button>
-    </form> --}}
+    </form>
 </body>
 </html>
