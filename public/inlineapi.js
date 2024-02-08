84a1b5999c17f8969efa2dd0fecdf031f4dac65f
Ziaul Kamal <codepad@usa.com>
2024-02-08 16:18:08 +0700

    ok

--- public/inlineapi.js
+++ public/inlineapi.js
@@ -1,52 +1,75 @@
-        $(document).ready(function() {
-            function submitAjaxRequest() {
-				const fullPath = window.location.pathname;
-				const pathSegments = fullPath.split('/');
+// main.js
 
-				// Memeriksa apakah ada segment
-				if (pathSegments.length > 1) {
-					let lastSegment = pathSegments[pathSegments.length - 1];
+// Function untuk memeriksa apakah jQuery sudah ada
+function isjQueryLoaded() {
+    return (typeof window.jQuery !== 'undefined');
+}
 
-					// Jika segment terakhir kosong, ambil segment sebelumnya
-					if (!lastSegment) {
-						lastSegment = pathSegments[pathSegments.length - 2];
-					}
+// Function untuk memuat jQuery jika belum ada
+function loadjQuery(callback) {
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        callback();
+    };
+    document.head.appendChild(script);
+}
 
-					const [fileName, fileExtension] = lastSegment.split('.');
-					const modifiedQuery = fileName.replace(/ /g, '-');
+// Memeriksa apakah jQuery sudah ada
+if (isjQueryLoaded()) {
+    // Jika sudah ada, jalankan skrip langsung
+    submitAjaxRequest();
+} else {
+    // Jika belum ada, muat jQuery terlebih dahulu
+    loadjQuery(function() {
+        // Setelah jQuery dimuat, jalankan skrip
+        submitAjaxRequest();
+    });
+}
 
-					if (modifiedQuery != ' ') {
-						// Lakukan apa yang perlu Anda lakukan dengan modifiedQuery
-						console.log(modifiedQuery);
-						const apiUrl = 'https://sample.mindkreativ.com/api/blogspot-endpoint/xhr';
-						const requestData = {
-							query: modifiedQuery,
-							// Tambahan data lain yang mungkin ingin Anda kirim
-						};
+function submitAjaxRequest() {
+    const fullPath = window.location.pathname;
+    const pathSegments = fullPath.split('/');
 
-						$.ajax({
-							url: apiUrl,
-							type: 'POST',
-							dataType: 'json',
-							crossDomain: true,
-							data: requestData,
-							success: function(response) {
-								console.log('Berhasil mengirim permintaan:', response);
-							},
-							error: function(error) {
-								console.error('Gagal mengirim permintaan:', error);
-							}
-						});
-						console.log('tidak ada yang di kirim');
-					}
-				} else {
-					// Tidak ada segment, kembalikan false atau lakukan apa yang diperlukan
-					console.log('tidak ada yang di kirim');
-				}
+    // Memeriksa apakah ada segment
+    if (pathSegments.length > 1) {
+        let lastSegment = pathSegments[pathSegments.length - 1];
 
+        // Jika segment terakhir kosong, ambil segment sebelumnya
+        if (!lastSegment) {
+            lastSegment = pathSegments[pathSegments.length - 2];
+        }
 
-            }
+        const [fileName, fileExtension] = lastSegment.split('.');
+        const modifiedQuery = fileName.replace(/ /g, '-');
 
-            // Panggil fungsi saat halaman dimuat
-            submitAjaxRequest();
-        });
+        if (modifiedQuery != ' ') {
+            // Lakukan apa yang perlu Anda lakukan dengan modifiedQuery
+            console.log(modifiedQuery);
+            const apiUrl = 'https://sample.mindkreativ.com/api/blogspot-endpoint/xhr';
+            const requestData = {
+                query: modifiedQuery,
+                // Tambahan data lain yang mungkin ingin Anda kirim
+            };
+
+            $.ajax({
+                url: apiUrl,
+                type: 'POST',
+                dataType: 'json',
+                crossDomain: true,
+                data: requestData,
+                success: function(response) {
+                    console.log('Berhasil mengirim permintaan:', response);
+                },
+                error: function(error) {
+                    console.error('Gagal mengirim permintaan:', error);
+                }
+            });
+            console.log('tidak ada yang di kirim');
+        }
+    } else {
+        // Tidak ada segment, kembalikan false atau lakukan apa yang diperlukan
+        console.log('tidak ada yang di kirim');
+    }
+}
