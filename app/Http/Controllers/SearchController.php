0ecd79ce805f92de19d42ea058d36e7ce4d4dea9
Ziaul Kamal <codepad@usa.com>
2024-02-08 11:16:48 +0700

    update

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->input('query');
+        dd($query);
+    }
+}
