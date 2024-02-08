a56d8e0523e5fc9f947cd0e78463d34b8f5a37db
Ziaul Kamal <codepad@usa.com>
2024-02-08 12:14:14 +0700

    ok

--- app/Http/Controllers/SubdomainController.php
+++ app/Http/Controllers/SubdomainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Request as FacadesRequest;
 use Illuminate\Support\Str;
 
 class SubdomainController extends Controller
